b644a3bf58b589bbf9ea480e094b24666407db78
bluepnwage <a.carty2555@gmail.com>
2023-03-08 18:35:36 -0400

    customize nextra theme

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,22 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>SXM COM Audit</span>,
   project: {
     link: "https://github.com/bluepnwage/sxm-com"
   },
-  // ...
-  primaryHue: { dark: 200 }
+  primaryHue: 150,
+  footer: {
+    text: (
+      <span>
+        Built by{" "}
+        <a
+          style={{ textDecoration: "underline" }}
+          href="https://bluepnwage-portfolio.vercel.app"
+          target="_blank"
+        >
+          Agis Carty
+        </a>
+        , <time dateTime={new Date().toString()}>{new Date().getFullYear()}</time>.
+      </span>
+    )
+  }
 };
