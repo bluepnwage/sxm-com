ea4092661ff70059d69e54d02f6c8d9e46c06848
bluepnwage <a.carty2555@gmail.com>
2023-03-08 19:03:17 -0400

    use middleware to redirect from index route to http route to avoid 404

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+export default function middleware(req: NextRequest) {
+  const url = new URL("http", req.nextUrl);
+  if (req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(url);
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/"
+};
