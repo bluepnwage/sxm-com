e342490fc49684e5ecd6168e33c1e1f30b23ec5b
bluepnwage <a.carty2555@gmail.com>
2023-03-09 18:27:38 -0400

    refactor content structure and add content to personal remarks

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
 export default function middleware(req: NextRequest) {
   const url = new URL("http", req.nextUrl);
   if (req.nextUrl.pathname === "/") {
